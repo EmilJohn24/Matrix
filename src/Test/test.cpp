9c40e836409f15e7571e2ba79af205fd5edd6cb3
Emil John Lopez <emiljohn24@gmail.com>
2019-09-09 20:34:51 +0800

    New test

--- src/Test/test.cpp
+++ src/Test/test.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <list>
 int main(){
-    Matrix<int> mat(100, 100, 10);
-    Matrix<int> mat2(100, 100, 2);
-    Matrix<double> mat3(100, 100, 2.0);
-    Matrix<double> mat4(200, 200, 3.0);
-    mat.multiply(mat2);
-    mat3.multiply(mat4);
+    Matrix<int> mat(4, 4, 10);
+    Matrix<int> mat2(4, 4, 2);
+    mat = mat.multiply(mat2);
+    mat.print(std::cout);
+    mat = mat.multiply(2);
+    mat.print(std::cout);
     //mat.colPrint(std::cout);
     std::cout<<"Done";
 
